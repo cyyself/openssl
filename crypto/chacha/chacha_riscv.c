22bb47f58e8fe8599f9c92eca05c045d46107f7b
Jerry Shih <bignose1007@gmail.com>
2024-03-09 15:03:56 +0800

    Use scalar ALU and vector ALU together for chacha20 stream cipher.
    Fixes #24070
    
    Use scalar ALU for 1 chacha block with rvv ALU simultaneously.
    The tail elements(non-multiple of block length) will be handled by
    the scalar logic.
    
    Use rvv path if the input length > chacha_block_size.
    
    And we have about 1.2x improvement comparing with the original code.

--- crypto/chacha/chacha_riscv.c
+++ crypto/chacha/chacha_riscv.c
@@ -47,7 +47,8 @@ void ChaCha20_ctr32_zvkb(unsigned char *out, const unsigned char *inp,
 void ChaCha20_ctr32(unsigned char *out, const unsigned char *inp, size_t len,
                     const unsigned int key[8], const unsigned int counter[4])
 {
-    if (RISCV_HAS_ZVKB() && riscv_vlen() >= 128) {
+    if (len > CHACHA_BLK_SIZE && RISCV_HAS_ZVKB() && RISCV_HAS_ZBB() &&
+        riscv_vlen() >= 128) {
         ChaCha20_ctr32_zvkb(out, inp, len, key, counter);
     } else {
         ChaCha20_ctr32_c(out, inp, len, key, counter);
